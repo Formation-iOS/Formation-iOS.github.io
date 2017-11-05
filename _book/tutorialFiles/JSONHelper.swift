50302ee2f629a8d569c4db2227d6a52332e543d3
CedricSoubrie <cedric.soubrie@gmail.com>
2017-11-05 22:53:16 +0100

    Nouvel export

---
+++ _book/tutorialFiles/JSONHelper.swift
@@ -0,0 +1,51 @@
+//
+//  JSONHelper.swift
+//  Atelier1_Parsing
+//
+//  Created by CedricSoubrie on 12/10/2017.
+//  Copyright © 2017 CedricSoubrie. All rights reserved.
+//
+
+import Foundation
+
+typealias JSON = [String:Any]
+
+class JSONHelper {
+    
+    static func jsonArray (fromJSONFile fileName:String) -> [JSON] {
+        guard let filePath = Bundle.main.path(forResource: fileName, ofType: "json") else {
+            print ("No local json files for path \(fileName)")
+            return []
+        }
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: filePath), options: [])
+            let json = try JSONSerialization.jsonObject(with: data, options: [])
+            if let array = json as? [JSON] {
+                return array
+            }
+            else {
+                print("\(fileName) doens't contain an array")
+                return []
+            }
+        }
+        catch {
+            print("Could not get json from file, make sure that file \(fileName) contains valid json.")
+            return []
+        }
+    }
+    
+    static func jsonData (fromJSONFile fileName:String) -> Data? {
+        guard let filePath = Bundle.main.path(forResource: fileName, ofType: "json") else {
+            print ("No local json files for path \(fileName)")
+            return nil
+        }
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: filePath), options: [])
+            return data
+        }
+        catch {
+            print ("Error while reading file \(fileName)")
+            return nil
+        }
+    }
+}
